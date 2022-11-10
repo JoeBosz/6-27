cd132abdbb3c28479798a394df60f0cf4f2947d8
Manav Misra <manavm@visionwebsoft.com>
2022-11-10 16:55:05 -0600

    Move sample HTML into DOM service

---
+++ src/services/dom.service.js
@@ -0,0 +1,22 @@
+/* <h2>Showing results for baseball</h2>
+          <section>
+            <h3>
+              Maps: Bach to Baseball Cards - Conservation Highlights
+              (Preservation, Library of Congress)
+            </h3>
+
+            <ul>
+              <li><span>Date</span>1749</li>
+              <li>
+                <span>Subjects</span>alexandria, library of congress,
+                preservation, world map, maps, conservation, library
+                collections, globes, george washington, geography
+              </li>
+              <li>
+                <span>Description</span>Maps - Bach to Baseball Cards:
+                Preserving the Nation's Heritage at the Library of Congress
+              </li>
+            </ul>
+
+            <a href="https://www.loc.gov/item/2017667579/">Read More</a>
+          </section> */
