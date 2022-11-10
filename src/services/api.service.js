f3d018cc7ffd0265f1a848e67cb1e9c63c39cd19
Manav Misra <manavm@visionwebsoft.com>
2022-11-10 16:55:21 -0600

    Add starter comments/code for API service

---
+++ src/services/api.service.js
@@ -0,0 +1,9 @@
+/**
+ * Example queries:
+ *
+ * - https://www.loc.gov/search/?fo=json&q=baseball
+ * - https://www.loc.gov/maps/?fo=json&q=baseball
+ * - https://www.loc.gov/photos/?fo=json&q=baseball
+ */
+
+const BASE_URL = "https://www.loc.gov";
